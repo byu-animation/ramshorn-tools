63ef128154c7835b9fa508e8d79d97460206dfbf
jmoborn <jmoborn@yahoo.com>
2014-03-08 13:48:04 -0700

    use updated software

--- project_env.sh
+++ project_env.sh
@@ -63,7 +63,7 @@ export OTLS_DIR=${PRODUCTION_DIR}/otls
 export HTOOLS_DIR=${PROJECT_TOOLS}/houdini-tools
 
 # Append to python path so batch scripts can access our modules
-export PYTHONPATH=/usr/autodesk/maya2012-x64/lib/python2.6/site-packages:/usr/lib64/python2.6/site-packages:${PROJECT_TOOLS}:${PROJECT_TOOLS}/asset_manager:${PROJECT_TOOLS}/houdini-tools/python2.6libs:${PROJECT_TOOLS}/nuke-tools/python:${PYTHONPATH}
+export PYTHONPATH=/usr/autodesk/maya2014-x64/lib/python2.7/site-packages:/usr/lib64/python2.6/site-packages:${PROJECT_TOOLS}:${PROJECT_TOOLS}/asset_manager:${PROJECT_TOOLS}/houdini-tools/python2.6libs:${PROJECT_TOOLS}/nuke-tools/python:${PYTHONPATH}
 
 # Issue submission website
 export ISSUE_URL="https://docs.google.com/forms/d/1bX1TSz9apCY4SuRkXpvtlb4pcdsmGEB9iHUwqyo4GwU/viewform"
