c79973c0cd1865d153183707e25490e20f6066e7
jmoborn <jmoborn@yahoo.com>
2014-03-13 00:04:19 -0600

    small bug

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 #This script sets permissions in a few directories 
 #that have been giving us trouble.
 
-source project_env.sh
+DIR=`dirname $0`
+source ${DIR}/project_env.sh
 chmod 774 -R "$USER_DIR"
 
 for a in "$ASSETS_DIR"/*
