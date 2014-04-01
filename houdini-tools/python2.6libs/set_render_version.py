daeee419456e10c5bac8a8f8a865a49154593d3b
jmoborn <jmoborn@yahoo.com>
2014-03-31 23:55:24 -0600

    set version function for rendering works

--- houdini-tools/python2.6libs/set_render_version.py
+++ houdini-tools/python2.6libs/set_render_version.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import re
 
 '''
 filepath is a valid path to a versioned render folder
@@ -8,15 +9,33 @@ if it is empty, it returns it
 otherwise creates the next version folder and returns it
 '''
 def set_version(filepath):
-    filepath = os.path.join(filepath, '*')
-    versions = glob.glob(filepath)
+    searchpath = os.path.join(filepath, '*')
+    files = glob.glob(searchpath)
+    versions = [f for f in files if os.path.isdir(f)]
     versions.sort
-    latest =  versions[-1]
-    if not os.listdir(latest):
-        return os.path.abspath(latest)
-    latest_int = int(latest[-1:])+1
-    latest = latest[:-1]+str(latest_int)
+    length = len(versions)
+    if length > 0:
+        latest = versions[-1]
+        index = 1
+        v = os.path.basename(latest)
+        # make sure it is a versioned folder
+        while  not re.match('v[0-9]+', v) and index < length:
+            latest = versions[-1-index]
+            v = os.path.basename(latest)
+            index += 1
+        if index < length:
+            # if it's empty use it
+            if not os.listdir(latest):
+                return latest
+        else:
+            v = 'v000'
+    else:
+        v = 'v000'
+    
+    latest_int = int(v[1:])+1
+    latest = os.path.join(filepath,'v'+'%03d'%latest_int)
+
     os.mkdir(latest)
-    return os.path.abspath(latest)
+    return latest
     
 print set_version("version_render")
