0c9f9b3b0a0d360e68092a6a04408483eda42725
Nathan Standiford <ns@byu.edu>
2014-09-24 13:40:46 -0600

    fixed bad env export

--- project_env.sh
+++ project_env.sh
@@ -20,7 +20,7 @@ if [ -z "$JOB" ]
 then
     # The name of the project (eg: owned)
     export PROJECT_NAME=ramshorn
-    export JOB=~/ramshorn/ramshorngt/${PROJECT_NAME}
+    export JOB=/groups/${PROJECT_NAME}
 
 else
     export PROJECT_NAME=`basename $JOB`
