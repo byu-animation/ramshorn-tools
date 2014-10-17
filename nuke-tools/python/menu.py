0d0f30c0d3f19897722642212c8dbd5eab47dd01
Nathan Standiford <ns@byu.edu>
2014-10-17 12:43:53 -0600

    added nuke shelf tools

--- nuke-tools/python/menu.py
+++ nuke-tools/python/menu.py
@@ -3,10 +3,15 @@ import checkin_Nuke as ci
 import checkout_Nuke as co
 import rollback_Nuke as rb
 import discard_Nuke as ds
+import autocomp_v16 as autocomp
+import os
+
+
+# methods called in menu items
 
 def checkout():
 	# nuke.message('check out')
-	co.go()
+    co.go()
 
 def checkin():
 	# nuke.message('check in')
@@ -16,10 +21,27 @@ def discard():
 	ds.go()
 
 def rollback():
-	rb.go()
+    rb.go()
 
-nuke.menu( 'Nuke' ).addCommand( 'Ramshorn/check out', 'checkout()')
-nuke.menu( 'Nuke' ).addCommand( 'Ramshorn/check in', 'checkin()')
-nuke.menu( 'Nuke' ).addCommand( 'Ramshorn/discard', 'discard()')
-nuke.menu( 'Nuke' ).addCommand( 'Ramshorn/rollback', 'rollback()')
+def autocomp():
+    autocomp.go()
+
+# get menubar
+menubar = nuke.menu('Nuke')
+
+# add menu items
+menubar.addCommand( 'Ramshorn/check out', 'checkout()')
+menubar.addCommand( 'Ramshorn/check in', 'checkin()')
+menubar.addCommand( 'Ramshorn/discard', 'discard()')
+menubar.addCommand( 'Ramshorn/rollback', 'rollback()')
+#nuke.menu( 'Nuke' ).addCommand( 'Ramshorn/autocomp', 'autocomp()')
+
+# get toolbar
+toolbar = nuke.toolbar("Nodes")
+
+# add custom ramshorn toolbar
+m = toolbar.addMenu("Nukeshorn", icon="ramshorn.png")
+m.addCommand("Andre Masks", "nuke.createNode(\"andre_masks.gizmo\")", icon="andre.png")
+m.addCommand("Papa Masks", "nuke.createNode('papa_masks.gizmo')", icon="papa.png")
+m.addCommand("Rambo Masks", "nuke.createNode('rambo_masks.gizmo')", icon="rambo.png")
 
